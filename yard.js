f5299878f8a17724a7aba28b04a8e43180202daf
rocnogu <rocnogu@abv.bg>
2022-01-08 13:19:39 +0100

    sec

--- yard.js
+++ yard.js
@@ -1,9 +1,8 @@
 function yard(input) {
   let sqM = input;
   let price = sqM * 7.61;
-  let discount = (price / 100) * 18;
-  let num = price - discount;
-  let finalPrice = num.toFixed(1);
+  let discount = price * 0.18;
+  let finalPrice = price - discount;
 
   console.log(`The final price is: ${finalPrice} lv.`);
   console.log(`The discount is: ${discount} lv.`);
