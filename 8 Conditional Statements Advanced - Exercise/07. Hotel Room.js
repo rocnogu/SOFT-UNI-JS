be4e4d1766c04f7d6bebf3e5c0f108d020fe2fc8
rocnogu <rocnogu@abv.bg>
2022-02-09 00:56:42 +0100

    8 Conditional Statements Advanced - Exercise / 8

--- 8 Conditional Statements Advanced - Exercise/07. Hotel Room.js
+++ 8 Conditional Statements Advanced - Exercise/07. Hotel Room.js
@@ -1,12 +1,91 @@
 function hotelRoom(input) {
+  // Вход
+  const month = input[0];
+  const nights = Number(input[1]);
   //
-  const degrees = Number(input[0]);
-  let timeOfDay = input[1];
+  let apartmentPrice = 0;
+  let studioPrice = 0;
   //
-  let outfit = "";
-  let shoes = "";
+  switch (month) {
+    case "May":
+    case "October":
+      studioPrice = 50;
+      apartmentPrice = 65;
+      if (nights > 7 && nights <= 14) {
+        studioPrice *= 0.95;
+      } else if (nights > 14) {
+        studioPrice *= 0.7;
+      } else {
+        console.log(`Error: nights`);
+      }
+      break;
+    case "June":
+    case "September":
+      studioPrice = 75.2;
+      apartmentPrice = 68.7;
+      if (nights > 14) {
+        studioPrice *= 0.8;
+      }
+      break;
 
-  console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+    case "July":
+    case "August":
+      studioPrice = 76;
+      apartmentPrice = 77;
+      break;
+  }
+  if (nights > 14) {
+    apartmentPrice *= 0.9;
+  }
+  //
+  let apartmentFinalPrice = apartmentPrice * nights;
+  let studioFinalPrice = studioPrice * nights;
+  //
+  console.log(`Apartment: ${apartmentFinalPrice.toFixed(2)} lv.`);
+  console.log(`Studio: ${studioFinalPrice.toFixed(2)} lv.`);
   //
 }
-hotelRoom;
+hotelRoom(["August", "20"]);
+
+function hotelRoom2(input) {
+  // Вход
+  const month = input[0];
+  const nights = Number(input[1]);
+  //
+  let apartmentPrice = 0;
+  let studioPrice = 0;
+  //
+  if (month == "May" || month == "October") {
+    apartmentPrice = 65;
+    studioPrice = 50;
+    if (nights > 7 && nights <= 14) {
+      studioPrice *= 0.95;
+    } else if (nights > 14) {
+      studioPrice *= 0.7;
+    } else {
+      console.log(`Error: nights`);
+    }
+  } else if (month === "June" || month == "September") {
+    studioPrice = 75.2;
+    apartmentPrice = 68.7;
+    if (nights > 14) {
+      studioPrice *= 0.8;
+    }
+  } else if (month === "July" || month == "August") {
+    studioPrice = 76;
+    apartmentPrice = 77;
+  } else {
+    console.log(`Error: month`);
+  }
+  if (nights > 14) {
+    apartmentPrice *= 0.9;
+  }
+  //
+  let apartmentFinalPrice = apartmentPrice * nights;
+  let studioFinalPrice = studioPrice * nights;
+  //
+  console.log(`Apartment: ${apartmentFinalPrice.toFixed(2)} lv.`);
+  console.log(`Studio: ${studioFinalPrice.toFixed(2)} lv.`);
+  //
+}
+hotelRoom2(["sfdg", "0"]);
--- 8 Conditional Statements Advanced - Exercise/08. On Time for the Exam.js
+++ 8 Conditional Statements Advanced - Exercise/08. On Time for the Exam.js
@@ -1,12 +1,69 @@
 function onTimeExam(input) {
+  // Вход
+  let hours = Number(input[0]);
+  let minutes = Number(input[1]);
+  let arivalHour = Number(input[2]);
+  let arivalMinute = Number(input[3]);
   //
-  const degrees = Number(input[0]);
-  let timeOfDay = input[1];
+  if (arivalHour > 0) {
+    arivalHour *= 60;
+  }
+  if (hours > 0) {
+    hours *= 60;
+  }
+  let arivalTime = arivalHour + arivalMinute;
+  let time = hours + minutes;
+  let onTime = time - 30;
   //
-  let outfit = "";
-  let shoes = "";
+  let timeLeft = arivalTime - time;
+  //
+  if (arivalTime > time) {
+    // Изход
+    console.log(`Late`);
 
-  console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+    if (timeLeft >= 60) {
+      let hoursLetf = (timeLeft / 60) >> 0;
+      let minutesLetf = timeLeft % 60;
+      if (minutesLetf > 0) {
+        if (minutesLetf < 10) {
+          console.log(`${hoursLetf}:0${minutesLetf} hours after the start`);
+        } else {
+          console.log(`${hoursLetf}:${minutesLetf} hours after the start`);
+        }
+      } else {
+        console.log(`${hoursLetf} 00 hours after the start`);
+      }
+    } else {
+      console.log(`${timeLeft} minutes after the start`);
+    }
+  } else if (arivalTime < time && arivalTime >= onTime) {
+    // Изход
+    console.log(`On time`);
+    timeLeft *= -1;
+    console.log(`${timeLeft} minutes before the start`);
+  } else if (arivalTime == time) {
+    // Изход
+    console.log(`On time`);
+  } else {
+    // Изход
+    console.log(`Early`);
+    if (timeLeft <= -60) {
+      let hoursLetf = ((timeLeft *= -1) / 60) >> 0;
+      let minutesLetf = timeLeft % 60;
+      if (minutesLetf > 0) {
+        if (minutesLetf < 10) {
+          console.log(`${hoursLetf}:0${minutesLetf} hours before the start`);
+        } else {
+          console.log(`${hoursLetf}:${minutesLetf} hours before the start`);
+        }
+      } else {
+        console.log(`${hoursLetf} 00 hours before the start`);
+      }
+    } else {
+      console.log(`${(timeLeft *= -1)} minutes before the start`);
+    }
+  }
+
   //
 }
-onTimeExam;
+onTimeExam(["14", "00", "13", "55"]);
