f73b914a9048bc174b743e23b6d40ddc2afeb4da
rocnogu <rocnogu@abv.bg>
2022-02-09 01:45:51 +0100

    8 Conditional Statements Advanced - Exercise / finished

--- 8 Conditional Statements Advanced - Exercise/09. Ski Trip.js
+++ 8 Conditional Statements Advanced - Exercise/09. Ski Trip.js
@@ -1,12 +1,46 @@
 function skiTrip(input) {
+  //// Вход
+  let dayOfStay = Number(input[0]);
+  let typeRoom = input[1];
+  // •	Трети ред - оценка - "positive"  или "negative"
+  let rating = input[2];
   //
-  const degrees = Number(input[0]);
-  let timeOfDay = input[1];
+  let priceRoom = 0;
   //
-  let outfit = "";
-  let shoes = "";
+  switch (typeRoom) {
+    case "room for one person":
+      priceRoom = 18;
+      break;
+    case "apartment":
+      priceRoom = 25;
+      if (dayOfStay < 10) {
+        priceRoom *= 0.7;
+      } else if (dayOfStay >= 10 && dayOfStay <= 15) {
+        priceRoom *= 0.65;
+      } else {
+        priceRoom *= 0.5;
+      }
+      break;
+    case "president apartment":
+      priceRoom = 35;
+      if (dayOfStay < 10) {
+        priceRoom *= 0.9;
+      } else if (dayOfStay >= 10 && dayOfStay <= 15) {
+        priceRoom *= 0.85;
+      } else {
+        priceRoom *= 0.8;
+      }
+      break;
+  }
 
-  console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+  if (rating == "positive") {
+    priceRoom *= 1.25;
+  } else {
+    priceRoom *= 0.9;
+  }
+  // Изход
+  let price = (dayOfStay - 1) * priceRoom;
+  console.log(price.toFixed(2));
   //
 }
-skiTrip;
+skiTrip(["2", "apartment", "positive"]);
